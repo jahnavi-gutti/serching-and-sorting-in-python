6c49dae34ec65686a445e1919c636aeea5eeccd2
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-11-14 15:02:50 +0530

    Create binaryserch.py

---
+++ binaryserch.py
@@ -0,0 +1,11 @@
+def binaryserch(a,x,si,ei):
+    if si>ei:
+        return -1
+    mid=(si+ei)//2
+    if a[mid]==x:
+        return mid
+    elif a[mid]>x:
+        return binaryserch(a,x,si,mid-1)
+    else:
+        return binaryserch(a,x,mid+1,ei)
+print(binaryserch([1,3,5,7,9,11,13,15,16,17],3,0,9))
